a7779f91bc32812cfb28864107f430f5b3b812fb
unknown <tammycampoverde@gmail.com>
2020-09-21 11:01:38 -0500

    taskbox UI

---
+++ storybook-static/precache-manifest.bf7042195426018b35406829d10ddea4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d379ebb08eacf546037f1c9b5f6ba48",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.30c861aa1d0f04914d96.bundle.js"
+  },
+  {
+    "url": "runtime~main.30c861aa1d0f04914d96.bundle.js"
+  },
+  {
+    "revision": "3ce9f9cc094a9ea6a06b",
+    "url": "static/css/main.67471bcc.chunk.css"
+  },
+  {
+    "url": "vendors~main.30c861aa1d0f04914d96.bundle.js"
+  },
+  {
+    "url": "vendors~main.30c861aa1d0f04914d96.bundle.js.LICENSE.txt"
+  }
+]);
